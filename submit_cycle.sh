3b3da1e7f45ee6c7f6b9a0a208f1bca2441c0eb4
Tseganeh Z. Gichamo <zacctsega@yahoo.com>
2026-03-17 08:52:23 -0400

    Remove unnecessary exit command from submit_cycle.sh

--- submit_cycle.sh
+++ submit_cycle.sh
@@ -167,7 +167,7 @@ while [ $date_count -lt $cycles_per_job ]; do
             echo "land DA script failed"
             exit
         fi   
-exit
+
         ############################
         #  convert restarts from tile to vector
         ############################
