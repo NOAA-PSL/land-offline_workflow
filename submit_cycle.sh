e1880e8d122c0cb4305d37a19dff81def90b115a
yuanxue2870 <yuan.xue@noaa.gov>
2026-07-02 08:57:53 -0400

    remove flags to make it to work on gaea

--- submit_cycle.sh
+++ submit_cycle.sh
@@ -317,7 +317,7 @@ while [ $date_count -lt $cycles_per_job ]; do
             cd $MEM_WORKDIR
                 
             #TODO: modify NoahMP to have mpi-group for each ensemble member and compare runtimes
-            time srun '--export=ALL' --exclusive --label -K -n ${NPROC_NOMP} --mem-per-cpu ${SLURM_MEM_PER_CPU} $LSMexec   &
+            time srun '--export=ALL' --label -K -n ${NPROC_NOMP} $LSMexec   &
             # #-N1-1 --exclusive
     
             # TODO: Modify noahmp to exit with error code    
